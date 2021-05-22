a76234407a0dec20b78327b3227836ad81a0e791
Bhupendra Gehlot <buntydgeneration@gmail.com>
2021-05-22 19:19:04 +0530

    new

--- 16.cpp
+++ 16.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> numbers;
+typedef vector<int> numbers;
 
 int main(){
 
@@ -11,7 +11,38 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        
-    }
+        unsigned int n;
+        cin>>n;
 
+        for(unsigned int c = temp.size(); c<=n; c++){
+            auto expo = temp.back();
+            unsigned int carry = 0;
+
+            for(auto i : expo){
+                i = i*2 + carry;  //2 times all....
+
+                if(i>=10){ //normal maths if it is 2 digit, we will carry one 
+                    i = i - 10;
+                    carry = 1;
+                }
+                else{
+                    carry = 0;
+                }
+            }
+
+            if(carry != 0){  //if some carry left 
+                expo.push_back(carry);
+            }
+            
+            temp.push_back(expo); //put the result
+        }
+        
+        int sum=0;
+        for(auto i: temp[n]){
+            sum = sum + i;
+        }
+        cout<<sum<<endl;
+
+    }
+    return 0;
 }
