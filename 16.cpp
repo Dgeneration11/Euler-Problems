33bb9c722b5e1bcfcd2765144adda47d523fc52a
Bhupendra Gehlot <buntydgeneration@gmail.com>
2021-05-22 19:10:36 +0530

    ne

---
+++ 16.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> numbers;
+
+int main(){
+
+    vector<numbers> temp;
+    temp.push_back({1}); // adding 2^0 = 1
+
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
+
+}
